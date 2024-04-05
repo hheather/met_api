9d6966dcd712a10582e85bb51b35500e1a55b0ae
Heather Bell <hheatherbell@gmail.com>
2024-04-05 12:31:22 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a small script for interacting with the Met archive's search endpoint.
---
+++ scrape.py
@@ -0,0 +1,39 @@
+import requests
+from pathlib import Path
+
+URL = "https://collectionapi.metmuseum.org"
+SEARCH_STR = "female statue"
+
+def download_image(img_url):
+    remove_https = img_url.replace('https://', '')
+    file_name = Path(remove_https).name
+    file_path = Path(f'./assets/{file_name}')
+    image_data = requests.get(img_url).content
+    print(f'Checking if {file_path} exists...')
+    if not file_path.exists():
+        print(f'Downloading from {img_url}...')
+        with open(file_path, 'wb') as f:
+            f.write(image_data)
+
+def image_url(id):
+    response = requests.get(f"{URL}/public/collection/v1/objects/{id}")
+    response_json = response.json()
+    primary_url = response_json.get('primaryImage', None)
+    if not primary_url:
+        print(f'No primary URL found for id: {id}')
+    return primary_url
+
+def search_objs():
+    response = requests.get(f"{URL}/public/collection/v1/search", 
+                           params={'q': SEARCH_STR, 'hasImages': True})
+    response_json = response.json()
+    obj_ids = response_json['objectIDs']
+    print(f'Found {len(obj_ids)} object IDs from API')
+    return obj_ids
+
+if __name__ == "__main__":
+    ids = search_objs()
+    for id in ids:
+        url = image_url(id)
+        if url:
+            download_image(url)
